bc6d16e9c39bd28baccc09eee7c1cb7f1d5f9402
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2025-06-04 18:10:12 +0200

    Update IAudioRemixConveyor.cs

--- Runtime/Scripts/Rooms/Streaming/Audio/IAudioRemixConveyor.cs
+++ Runtime/Scripts/Rooms/Streaming/Audio/IAudioRemixConveyor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using LiveKit.Internal;
 using Livekit.Utils;
+using UnityEngine;
 
 namespace LiveKit.Rooms.Streaming.Audio
 {
@@ -20,10 +21,26 @@ namespace LiveKit.Rooms.Streaming.Audio
                 uint sampleRate
             )
             {
-                using var uFrame = resampler.RemixAndResample(ownedAudioFrame, numChannels, sampleRate);
-                var data = uFrame.AsSpan();
-                using var guard = outputBuffer.Lock();
-                guard.Value.Write(data);
+                // Optimization: Skip expensive resampling if formats already match
+                if (ownedAudioFrame.numChannels == numChannels && ownedAudioFrame.sampleRate == sampleRate)
+                {
+                    // Direct copy - no resampling needed
+                    Utils.Error($"AudioRemixConveyor: Direct copy {ownedAudioFrame.numChannels}ch@{ownedAudioFrame.sampleRate}Hz -> {numChannels}ch@{sampleRate}Hz");
+                    var data = ownedAudioFrame.AsSpan();
+                    using var guard = outputBuffer.Lock();
+                    guard.Value.Write(data);
+                    
+                    // Frame will be disposed when method exits
+                }
+                else
+                {
+                    // Resampling required - use FFI resampler
+                    Utils.Error($"AudioRemixConveyor: Resampling {ownedAudioFrame.numChannels}ch@{ownedAudioFrame.sampleRate}Hz -> {numChannels}ch@{sampleRate}Hz");
+                    using var uFrame = resampler.RemixAndResample(ownedAudioFrame, numChannels, sampleRate);
+                    var data = uFrame.AsSpan();
+                    using var guard = outputBuffer.Lock();
+                    guard.Value.Write(data);
+                }
             }
 
             public void Dispose()
