a6aafc1d8081b88b3fb941b4c748fc19f3dcd2f0
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2025-06-10 21:26:55 +0200

    Improvements to OptimizedRTCAudioSource

--- Runtime/Scripts/OptimizedRtcAudioSource.cs
+++ Runtime/Scripts/OptimizedRtcAudioSource.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Runtime.InteropServices;
+using System.Collections.Concurrent;
+using System.Numerics;
+using System.Buffers;
 using LiveKit.Internal;
 using LiveKit.Internal.FFIClients.Requests;
 using LiveKit.Proto;
@@ -15,25 +18,38 @@ namespace LiveKit
         private const float S16_MAX_VALUE = 32767f;
         private const float S16_MIN_VALUE = -32768f;
         private const float S16_SCALE_FACTOR = 32768f;
-        private readonly Mutex<RingBuffer> buffer;
+        private const int POOL_SIZE = 4;
+        private const int BATCH_SIZE = 4;
+        private const int VECTOR_SIZE = 4;
+
+        private readonly ConcurrentQueue<short[]> bufferPool;
+        private readonly ConcurrentQueue<short[]> writeQueue;
+        private readonly ConcurrentQueue<short[]> readQueue;
         private readonly object lockObject = new();
 
         private readonly IAudioFilter audioFilter;
-        private short[] tempBuffer;
         private AudioFrame frame;
         private uint channels;
         private uint sampleRate;
         private int currentBufferSize;
-
         private int cachedFrameSize;
-        public FfiHandle Handle => handle;
+        private readonly Vector4 scaleFactor;
+        private readonly Vector4 maxValue;
+        private readonly Vector4 minValue;
 
+        public FfiHandle Handle => handle;
         internal FfiHandle handle { get; }
 
         public OptimizedRtcAudioSource(IAudioFilter audioFilter)
         {
-            buffer = new Mutex<RingBuffer>(new RingBuffer(0));
+            bufferPool = new ConcurrentQueue<short[]>();
+            writeQueue = new ConcurrentQueue<short[]>();
+            readQueue = new ConcurrentQueue<short[]>();
             currentBufferSize = 0;
+            scaleFactor = new Vector4(S16_SCALE_FACTOR);
+            maxValue = new Vector4(S16_MAX_VALUE);
+            minValue = new Vector4(S16_MIN_VALUE);
+
             using var request = FFIBridge.Instance.NewRequest<NewAudioSourceRequest>();
             var newAudioSource = request.request;
             newAudioSource.Type = AudioSourceType.AudioSourceNative;
@@ -50,6 +66,27 @@ namespace LiveKit
             FfiResponse res = response;
             handle = IFfiHandleFactory.Default.NewFfiHandle(res.NewAudioSource.Source.Handle!.Id);
             this.audioFilter = audioFilter;
+        }
+
+        private short[] GetBuffer(int size)
+        {
+            if (bufferPool.TryDequeue(out var buffer) && buffer.Length >= size)
+            {
+                return buffer;
+            }
+            return ArrayPool<short>.Shared.Rent(size);
+        }
+
+        private void ReturnBuffer(short[] buffer)
+        {
+            if (bufferPool.Count < POOL_SIZE)
+            {
+                bufferPool.Enqueue(buffer);
+            }
+            else
+            {
+                ArrayPool<short>.Shared.Return(buffer);
+            }
         }
 
         public void Start()
@@ -69,118 +106,131 @@ namespace LiveKit
 
             lock (lockObject)
             {
-                using var guard = buffer.Lock();
-                guard.Value.Dispose();
+                while (writeQueue.TryDequeue(out var buffer))
+                {
+                    ReturnBuffer(buffer);
+                }
+                while (readQueue.TryDequeue(out var buffer))
+                {
+                    ReturnBuffer(buffer);
+                }
                 if (frame.IsValid) frame.Dispose();
+            }
+        }
+
+        private void ProcessVectorized(Span<float> input, Span<short> output)
+        {
+            int i = 0;
+            for (; i <= input.Length - VECTOR_SIZE; i += VECTOR_SIZE)
+            {
+                var vector = new Vector4(
+                    input[i],
+                    input[i + 1],
+                    input[i + 2],
+                    input[i + 3]
+                );
+                vector *= scaleFactor;
+                vector = Vector4.Clamp(vector, minValue, maxValue);
+                
+                output[i] = (short)MathF.Round(vector.X);
+                output[i + 1] = (short)MathF.Round(vector.Y);
+                output[i + 2] = (short)MathF.Round(vector.Z);
+                output[i + 3] = (short)MathF.Round(vector.W);
+            }
+
+            // Handle remaining elements
+            for (; i < input.Length; i++)
+            {
+                var sample = input[i] * S16_SCALE_FACTOR;
+                if (sample > S16_MAX_VALUE) sample = S16_MAX_VALUE;
+                else if (sample < S16_MIN_VALUE) sample = S16_MIN_VALUE;
+                output[i] = (short)(sample + (sample >= 0 ? 0.5f : -0.5f));
             }
         }
 
         private void OnAudioRead(Span<float> data, int channels, int sampleRate)
         {
             var needsReconfiguration = channels != this.channels ||
-                                       sampleRate != this.sampleRate ||
-                                       data.Length != tempBuffer?.Length;
+                                       sampleRate != this.sampleRate;
 
-            var newBufferSize = 0;
             if (needsReconfiguration)
             {
-                newBufferSize = (int)(channels * sampleRate * BUFFER_DURATION_S) * sizeof(short);
-            }
-
-            lock (lockObject)
-            {
-                if (needsReconfiguration)
+                lock (lockObject)
                 {
-                    var needsNewBuffer = newBufferSize != currentBufferSize;
-
-                    if (needsNewBuffer)
-                    {
-                        using var guard = buffer.Lock();
-                        guard.Value.Dispose();
-                        guard.Value = new RingBuffer(newBufferSize);
-                        currentBufferSize = newBufferSize;
-                    }
-
-                    tempBuffer = new short[data.Length];
                     this.channels = (uint)channels;
                     this.sampleRate = (uint)sampleRate;
                     if (frame.IsValid) frame.Dispose();
-                    frame = new AudioFrame(this.sampleRate, this.channels, (uint)(tempBuffer.Length / this.channels));
-
+                    frame = new AudioFrame(this.sampleRate, this.channels, (uint)(data.Length / this.channels));
                     cachedFrameSize = frame.Length;
                 }
+            }
 
-                if (tempBuffer == null)
-                {
-                    Utils.Error("Temp buffer is null");
-                    return;
-                }
+            var buffer = GetBuffer(data.Length);
+            ProcessVectorized(data, buffer);
+            writeQueue.Enqueue(buffer);
 
-                var tempSpan = tempBuffer.AsSpan();
-                for (var i = 0; i < data.Length; i++)
-                {
-                    var sample = data[i] * S16_SCALE_FACTOR;
-                    if (sample > S16_MAX_VALUE) sample = S16_MAX_VALUE;
-                    else if (sample < S16_MIN_VALUE) sample = S16_MIN_VALUE;
-                    tempSpan[i] = (short)(sample + (sample >= 0 ? 0.5f : -0.5f));
-                }
-
-                var shouldProcessFrame = false;
-                using (var guard = buffer.Lock())
-                {
-                    var audioBytes = MemoryMarshal.Cast<short, byte>(tempBuffer.AsSpan());
-                    guard.Value.Write(audioBytes);
-                    shouldProcessFrame = guard.Value.AvailableRead() >= cachedFrameSize;
-                }
-
-                if (shouldProcessFrame)
-                {
-                    ProcessAudioFrame();
-                }
+            if (writeQueue.Count >= BATCH_SIZE)
+            {
+                ProcessAudioFrames();
             }
         }
 
-        private void ProcessAudioFrame()
+        private void ProcessAudioFrames()
         {
             if (!frame.IsValid) return;
 
-            unsafe
+            while (writeQueue.TryDequeue(out var buffer))
             {
-                var frameSpan = new Span<byte>(frame.Data.ToPointer(), cachedFrameSize);
+                readQueue.Enqueue(buffer);
+            }
 
-                using (var guard = buffer.Lock())
+            var framesToProcess = Math.Min(readQueue.Count, BATCH_SIZE);
+            var frameBuffers = new short[framesToProcess][];
+
+            for (int i = 0; i < framesToProcess; i++)
+            {
+                if (readQueue.TryDequeue(out var buffer))
                 {
-                    var bytesRead = guard.Value.Read(frameSpan);
-                    if (bytesRead < cachedFrameSize)
-                    {
-                        return; // Don't send incomplete frames
-                    }
+                    frameBuffers[i] = buffer;
                 }
             }
 
-            try
+            for (int i = 0; i < framesToProcess; i++)
             {
-                using var request = FFIBridge.Instance.NewRequest<CaptureAudioFrameRequest>();
-                using var audioFrameBufferInfo = request.TempResource<AudioFrameBufferInfo>();
+                var buffer = frameBuffers[i];
+                unsafe
+                {
+                    var frameSpan = new Span<byte>(frame.Data.ToPointer(), cachedFrameSize);
+                    var audioBytes = MemoryMarshal.Cast<short, byte>(buffer.AsSpan());
+                    audioBytes.Slice(0, Math.Min(audioBytes.Length, cachedFrameSize)).CopyTo(frameSpan);
+                }
 
-                var pushFrame = request.request;
-                pushFrame.SourceHandle = (ulong)handle.DangerousGetHandle();
-                pushFrame.Buffer = audioFrameBufferInfo;
-                pushFrame.Buffer.DataPtr = (ulong)frame.Data;
-                pushFrame.Buffer.NumChannels = frame.NumChannels;
-                pushFrame.Buffer.SampleRate = frame.SampleRate;
-                pushFrame.Buffer.SamplesPerChannel = frame.SamplesPerChannel;
+                try
+                {
+                    using var request = FFIBridge.Instance.NewRequest<CaptureAudioFrameRequest>();
+                    using var audioFrameBufferInfo = request.TempResource<AudioFrameBufferInfo>();
 
-                using var response = request.Send();
+                    var pushFrame = request.request;
+                    pushFrame.SourceHandle = (ulong)handle.DangerousGetHandle();
+                    pushFrame.Buffer = audioFrameBufferInfo;
+                    pushFrame.Buffer.DataPtr = (ulong)frame.Data;
+                    pushFrame.Buffer.NumChannels = frame.NumChannels;
+                    pushFrame.Buffer.SampleRate = frame.SampleRate;
+                    pushFrame.Buffer.SamplesPerChannel = frame.SamplesPerChannel;
 
-                pushFrame.Buffer.DataPtr = 0;
-                pushFrame.Buffer.NumChannels = 0;
-                pushFrame.Buffer.SampleRate = 0;
-                pushFrame.Buffer.SamplesPerChannel = 0;
-            }
-            catch (Exception e)
-            {
-                Utils.Error("Audio Framedata error: " + e.Message + "\nStackTrace: " + e.StackTrace);
+                    using var response = request.Send();
+
+                    pushFrame.Buffer.DataPtr = 0;
+                    pushFrame.Buffer.NumChannels = 0;
+                    pushFrame.Buffer.SampleRate = 0;
+                    pushFrame.Buffer.SamplesPerChannel = 0;
+                }
+                catch (Exception e)
+                {
+                    Utils.Error("Audio Framedata error: " + e.Message + "\nStackTrace: " + e.StackTrace);
+                }
+
+                ReturnBuffer(buffer);
             }
         }
     }
