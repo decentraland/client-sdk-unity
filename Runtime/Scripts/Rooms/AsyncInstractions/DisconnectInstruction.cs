bb563f7d3656a10deabd1c0df79961b5a1b2fddc
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2026-03-03 16:50:47 -0300

    fix: Disconnect race condition
    Signed-off-by: Juan Ignacio Molteni <juanignaciomolteni@gmail.com>

--- Runtime/Scripts/Rooms/AsyncInstractions/DisconnectInstruction.cs
+++ Runtime/Scripts/Rooms/AsyncInstractions/DisconnectInstruction.cs
@@ -1,4 +1,5 @@
 using System.Threading;
+using System.Collections.Generic;
 using LiveKit.Internal;
 using LiveKit.Proto;
 
@@ -6,28 +7,56 @@ namespace LiveKit.Rooms.AsyncInstractions
 {
     public sealed class DisconnectInstruction : AsyncInstruction
     {
-        private readonly ulong asyncId;
+        private ulong asyncId;
         private readonly Room room;
+        private bool asyncIdSet;
+        private readonly List<ulong> receivedAsyncIds;
+        private readonly object _lock = new object();
 
-        internal DisconnectInstruction(ulong asyncId, Room room, CancellationToken token) : base(token)
+        internal DisconnectInstruction(Room room, CancellationToken token) : base(token)
         {
-            this.asyncId = asyncId;
             this.room = room;
+            this.asyncIdSet = false;
+            this.receivedAsyncIds = new List<ulong>();
             FfiClient.Instance.DisconnectReceived += OnDisconnect;
+        }
+
+        internal void SetAsyncId(ulong asyncId)
+        {
+            lock (_lock)
+            {
+                this.asyncId = asyncId;
+                asyncIdSet = true;
+
+                if (receivedAsyncIds.Contains(asyncId))
+                    Complete();
+            }
         }
 
         private void OnDisconnect(DisconnectCallback e)
         {
-            Utils.Debug($"OnConnect.... {e}");
-            if (asyncId != e.AsyncId)
-                return;
+            lock (_lock)
+            {
+                if (!asyncIdSet)
+                {
+                    receivedAsyncIds.Add(e.AsyncId);
+                    return;
+                }
 
+                if (asyncId != e.AsyncId)
+                    return;
+
+                Complete();
+            }
+        }
+
+        private void Complete()
+        {
             FfiClient.Instance.DisconnectReceived -= OnDisconnect;
-
             room.OnDisconnect();
-
             ErrorMessage = string.Empty;
             IsDone = true;
+            receivedAsyncIds.Clear();
         }
     }
 }
